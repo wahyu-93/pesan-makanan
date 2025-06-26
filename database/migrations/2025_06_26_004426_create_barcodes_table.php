bbb905ff602cc981bc8247248b9b61030c35a9d4
wahyu93 <wahyu.wahyudi1005@gmail.com>
2025-06-26 09:02:07 +0800

    model dan table [done]

---
+++ database/migrations/2025_06_26_004426_create_barcodes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barcodes', function (Blueprint $table) {
+            $table->id();
+            $table->string('table_number');
+            $table->images();
+            $table->string('qr_code');
+            $table->foreignId('users_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barcodes');
+    }
+};
---
+++ database/migrations/2025_06_26_004433_create_food_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('images');
+            $table->integer('price');
+            $table->integer('price_afterdiscount');
+            $table->integer('discount')->nullable();
+            $table->boolean('is_promo')->nullable()->default(true);
+            $table->foreignId('categories_id')->constrained('category')->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('food');
+    }
+};
